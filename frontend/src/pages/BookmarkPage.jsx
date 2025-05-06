cd48006b59a288727554fc4f3f1f67b59ee37e40
gimgyuwon <gimgyuwon2@gmail.com>
2025-05-06 13:30:40 -0700

    feat: 북마크 페이지 뼈대 구현

--- frontend/src/pages/BookmarkPage.jsx
+++ frontend/src/pages/BookmarkPage.jsx
@@ -1,12 +1,53 @@
+import AlertModal from "../components/Common/AlertModal/AlertModal";
 import useAuthStore from "../store/useAuthStore";
+import { useState } from "react";
+import getKakaoAuthURL from "../utils/kakaoAuthUrl";
+import { useNavigate } from "react-router-dom";
+import SubPolicy from "../components/Candidate/Common/SubPolicy";
+import { CANDIDATE_POLICY } from "../constant/Candidate/CandidatePolicy";
+import Chip from "../components/Common/Chip/Chip";
 
 export const BookmarkPage = () => {
-  const { user, bookmarks, toggleBookmark } = useAuthStore();
+  const navigate = useNavigate();
+  const { user, bookmarks } = useAuthStore();
+  const [alertOpen, setAlertOpen] = useState(!!user);
+  const [selectedTag, setSelectedTag] = useState("전체");
+
+  const handleLoginClick = () => {
+    window.location.href = getKakaoAuthURL();
+  };
+
+  const handleNextClick = () => {
+    navigate("/");
+  };
+
+  const subPolicy =
+    selectedTag === "전체"
+      ? CANDIDATE_POLICY
+      : CANDIDATE_POLICY.filter((item) => item.prop?.includes(selectedTag));
+
+  if (alertOpen) {
+    return (
+      <AlertModal
+        message="로그인이 필요한 화면입니다"
+        buttonMessage="로그인"
+        onButtonClick={handleLoginClick}
+        secondButtonMessage="나중에"
+        onSecondButtonClick={handleNextClick}
+      />
+    );
+  }
 
   return (
-    <div>
-      유저
-      <div>{user?.nickname}</div>
+    <div className="space-y-4">
+      <div className="text-[18px] py-1">
+        북마크한 공약 {bookmarks.length || 0}개
+      </div>
+      <Chip select={selectedTag} onSelect={setSelectedTag} />
+      <SubPolicy
+        policy={bookmarks.length ? bookmarks : subPolicy}
+        compare={true}
+      />
     </div>
   );
 };
