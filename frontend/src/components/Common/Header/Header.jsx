c3b3cf40a5dc0d3503d8f6ed2bb5fdaa8b41fb23
gimgyuwon <gimgyuwon2@gmail.com>
2025-05-07 18:02:29 -0700

    feat: 헤더 북마크 active 디자인 적용

--- frontend/src/components/Common/Header/Header.jsx
+++ frontend/src/components/Common/Header/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { CandidateSelect } from "./CandidateSelect/CandiateSelect";
 import bookmarker from "../../../assets/icons/bookmarker.svg";
+import focusBookmarker from "../../../assets/icons/focusBookmarker.svg";
 import darkMode from "../../../assets/icons/darkMode.svg";
 
 export const Header = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const isActive = location.pathname == "/bookmark";
   const handleTitleClick = () => {
     navigate("/");
   };
@@ -22,13 +25,13 @@ export const Header = () => {
           </div>
           <div className="flex flex-row space-x-4">
             <img
-              src={bookmarker}
+              src={isActive ? focusBookmarker : bookmarker}
               alt="bookmarker"
-              width={13}
-              height={13}
+              width={15}
+              height={15}
               onClick={() => handleBookmarkClick()}
             />
-            <img src={darkMode} alt="darkMode" width={30} height={30} />
+            <img src={darkMode} alt="darkMode" width={35} height={35} />
           </div>
         </div>
       </div>
